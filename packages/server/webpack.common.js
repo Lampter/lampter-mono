72f7e2cdbe0f0a82c1475bb71ef69af6df6ada43
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-11-04 19:32:39 -0500

    removed useless

--- packages/server/webpack.common.js
+++ packages/server/webpack.common.js
@@ -1,11 +1,8 @@
 const { join, resolve } = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
 const monorepoRoot = join(__dirname, "..", "..");
 
 module.exports = {
-  externals: [nodeExternals({})],
-  target: "node",
   output: {
     filename: "index.js",
     path: join(__dirname, "umd")
