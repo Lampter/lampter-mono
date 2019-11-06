f8d3cf7a192cc5fa35b2fed5f499d8c37d6dad59
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-11-05 22:55:25 -0500

    other update

--- packages/github-app/src/index.ts
+++ packages/github-app/src/index.ts
@@ -101,8 +101,6 @@ export = (app: Application) => {
     async (context: Context<Webhooks.WebhookPayloadPullRequest & {[key:string]: any}>) => {
       // TODO: Pull Request Data Formating
       const {pull_request, repository, sender} = context.payload
-     
-      app.log(context.payload)
 
       const payload: Partial<PullRequestPayload> = {
         repository: {
@@ -142,7 +140,7 @@ export = (app: Application) => {
 
       event = {...event, payload}
 
-      // app.log(event)
+      app.log(event)
     },
   );
   app.on(
