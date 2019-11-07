51cd70e9dc4035385d245e6fab49bf200a761781
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-11-07 13:33:35 -0500

    moved to using event_action

--- packages/server/src/resolvers/Event.resolver.ts
+++ packages/server/src/resolvers/Event.resolver.ts
@@ -20,9 +20,6 @@ class CreateEventInputArgs {
   public applicationId!: number;
 
   @Field()
-  public reference!: string;
-
-  @Field()
   public action!: EventAction;
 
   @Field()
@@ -50,15 +47,26 @@ export default class EventResolver {
   public async createEvent(@Args()
   {
     applicationId,
-    reference,
     action,
     payload,
   }: CreateEventInputArgs) {
-    let referenceId: number;
+    let referenceId: number | undefined = undefined;
+    let reference: string | undefined = undefined;
     // FIXME: Use action directly instead of passing the reference ?
-    switch (reference) {
+    switch (action) {
       // Deal With pull_request events
-      case "pull_request": {
+      case EventAction.PULL_REQUEST__ASSIGNED:
+      case EventAction.PULL_REQUEST__CLOSED:
+      case EventAction.PULL_REQUEST__EDITED:
+      case EventAction.PULL_REQUEST__LABELED:
+      case EventAction.PULL_REQUEST__OPENED:
+      case EventAction.PULL_REQUEST__REOPENED:
+      case EventAction.PULL_REQUEST__REVIEW_REQUESTED_REMOVED:
+      case EventAction.PULL_REQUEST__REVIEW_REQUESTED:
+      case EventAction.PULL_REQUEST__UNASSIGNED:
+      case EventAction.PULL_REQUEST__UNLABELED:
+      case EventAction.PULL_REQUEST__SYNCHRONIZE: {
+        reference = "pull_request";
         const { pullRequest: pr }: any = JSON.parse(payload);
         const pullRequest = await PullRequest.findOne({
           where: { originalId: pr.originalId, applicationId: pr.applicationId },
@@ -81,9 +89,10 @@ export default class EventResolver {
       // case "issue": {
       //   break;
       // }
-      default: {
-        referenceId = 10000;
-      }
+    }
+
+    if (!reference || !referenceId) {
+      throw new Error("Event Not Supported yet...");
     }
 
     const newEvent = Event.create({
